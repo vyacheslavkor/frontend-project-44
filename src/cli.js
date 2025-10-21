71db695c95ddd5c3e0c12705ecf2a7774e44d4af
Vyacheslav Korolev <slavakorolev2@gmail.com>
2025-10-21 21:23:46 +0400

    Linter added.

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-export default () => {
-    const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-    console.log(`Hello, ${name}!`)
+const welcome = () => {
+  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ')
+  console.log(`Hello, ${name}!`)
 }
+export default welcome
